0b8addb2f348129d94e4b2a950956fbd5f1d5275
Sherry Martin <sm674@cornell.edu>
2018-09-16 13:19:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,8 @@ def take_a_number(deli, name)
 end 
 
 def now_serving(deli)
-  if .shift
+  if 
+    deli.shift
     puts "Currently serving #{name}."
   else
     puts "There is nobody waiting to be served!"
