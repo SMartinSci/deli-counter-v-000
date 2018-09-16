df44e68d081e6892dc6802628caa088774c485f6
Sherry Martin <sm674@cornell.edu>
2018-09-16 13:18:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,8 +17,8 @@ def take_a_number(deli, name)
   puts "Welcome, #{name}. You are number #{deli.length} in line."
 end 
 
-def now_serving
-  if
+def now_serving(deli)
+  if .shift
     puts "Currently serving #{name}."
   else
     puts "There is nobody waiting to be served!"
