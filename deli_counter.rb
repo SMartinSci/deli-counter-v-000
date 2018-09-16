6f4bccc09def6040ac4ee2e801ca8c67b2c901aa
Sherry Martin <sm674@cornell.edu>
2018-09-16 13:27:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 
 def now_serving(deli)
   if deli.length > 0
-    puts "Currently serving #{deli}."
+    puts "Currently serving #{deli[0]}."
     deli.shift
   else
     puts "There is nobody waiting to be served!"
